c0fa86045a75b9a66a783a2b7fb2f2d151a93570
Bilal AMRI <iambilalamri@gmail.com>
2021-02-13 22:35:59 +0100

    [master]- learn new thing about microservices configuration

--- src/main/java/com/amrib/config/resource/StartUpController.java
+++ src/main/java/com/amrib/config/resource/StartUpController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,6 +32,9 @@ public class StartUpController {
 	@Autowired
 	private DBSettings dbSettings;
 
+	@Autowired
+	private Environment environment;
+	
 	@GetMapping("/startupone")
 	public String startup() {
 		return myGreeting + ": " + description + " = " + staticMsg + " = " + myValues + " **** " + mapValues;
@@ -39,5 +43,10 @@ public class StartUpController {
 	@GetMapping("/from-configurationProperty")
 	public String levelup() {
 		return dbSettings.getConnection() + ": [" + dbSettings.getHost() + ":" + dbSettings.getPort() + "]";
+	}
+	
+	@GetMapping("/envdetails")
+	public String envDetails() {
+		return environment.toString();
 	}
 }
