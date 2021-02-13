85af67217c278469d3139205af3eef0461cddb19
Bilal AMRI <iambilalamri@gmail.com>
2021-02-13 20:49:08 +0100

    initial commit

---
+++ src/main/java/com/amrib/config/ConfigureSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.amrib.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigureSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigureSpringProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amrib/config/ConfigureSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amrib.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigureSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
